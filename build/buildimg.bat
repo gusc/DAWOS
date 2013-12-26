1170a53bb311dc5c3a92d04fd5a48d0bd59f6b8a
Gusts Kaksis <gusts.kaksis@gmail.com>
2013-12-26 14:08:30 +0200

    Updated build scripts and test scripts

--- build/buildimg.bat
+++ build/buildimg.bat
@@ -2,11 +2,11 @@
 
 ::REM 10 tracs, 16 heads, 63 sectors-per-track = 10080 sectors
 SET maxbytes=5160960
-SET mbr=..\Release\mbr.img
-SET bbp=..\Release\bbp.img
-SET output=..\Release\disk.img
+SET mbr=..\bin\mbr.img
+SET bbp=..\bin\bbp.img
+SET output=..\bin\disk.img
 
-..\..\diskutils\Release\buildimg.exe -m %mbr% -b %bbp% -s %maxbytes% %output%
+buildimg.exe -m %mbr% -b %bbp% -s %maxbytes% %output%
 
 ECHO Cleanup
 ::DEL %mbr%
