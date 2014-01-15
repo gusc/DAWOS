851bbf1acb94ed5e58bdf40ac50b6bf16968a593
Gusts 'gusC' Kaksis <gusts.kaksis@graftonit.lv>
2014-01-15 20:56:29 +0200

    Added some CMOS register macros

--- src/boot/bios/bbp/loader/cmos.h
+++ src/boot/bios/bbp/loader/cmos.h
@@ -40,6 +40,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "common.h"
 #include "io.h"
 
+#define CMOS_RTC_SECONDS 0x00
+#define CMOS_RTC_MINUTES 0x02
+#define CMOS_RTC_HOURS 0x04
+#define CMOS_RTC_WEEKDAY 0x06
+#define CMOS_RTC_DAY 0x07
+#define CMOS_RTC_MONTH 0x08
+#define CMOS_RTC_YEAR 0x09
+#define CMOS_RTC_CENTURY 0x32
+#define CMOS_RTC_STATUS_A 0x0A
+#define CMOS_RTC_STATUS_B 0x0B
+
+#define CMOS_RTC_STATUS_24H 0x02
+#define CMOS_RTC_STATUS_BIN 0x04
+
 /**
 * Read CMOS register
 * @param reg - register number
